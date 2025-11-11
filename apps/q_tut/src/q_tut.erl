939d2d1cf7b5afd3e49dfd756a48af4c6ea9927f
ts-klassen <git@su-shiki.com>
2025-11-11 20:09:11 +0900

    Add tutorial error endpoint

--- apps/q_tut/src/q_tut.erl
+++ apps/q_tut/src/q_tut.erl
@@ -1,14 +1,30 @@
 -module(q_tut).
+-include_lib("qrpc/include/qrpc.hrl").
 
 -export([
         hello_world/1
+      , hello_error/1
       , hello_admin/1
     ]).
 
 hello_world(_) ->
     #{ payload => <<"hello world">> }.
 
+hello_error(Rpc) ->
+    ?QRPC_ERROR(#{
+        id => [q_tut, q_tut, hello_error]
+      , fault_source => client
+      , message => <<"hello error (An sample error)">>
+      , message_ja => <<"hello error （エラーのサンプルです）"/utf8>>
+      , detail => #{
+            rpc => Rpc:strip_rpc_for_log()
+        }
+      , is_known => true
+      , is_retryable => false
+      , version => 1
+    }).
+
 hello_admin(Rpc) ->
-    #{ payload => Rpc:get([payload]) }.
+    #{ payload => Rpc:get([metadata, jwt]) }.
 
 
