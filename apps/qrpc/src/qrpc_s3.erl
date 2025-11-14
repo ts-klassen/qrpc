cee9b2f3c5bc2eb1c25e7b2c5090f335547fa805
ts-klassen <git@su-shiki.com>
2025-11-15 04:55:19 +0900

    added SigV4 support

--- apps/qrpc/src/qrpc_s3.erl
+++ apps/qrpc/src/qrpc_s3.erl
@@ -6,11 +6,17 @@
 -export([
         create_bucket/4
       , list_buckets/2
+      , make_presigned_url/3
     ]).
 
 -export_type([
         config/0
       , access_key/0
+      , bucket/0
+      , key/0
+      , ttl/0
+      , http_method/0
+      , make_presigned_url_opts/0
     ]).
 
 -type access_key() :: #{
@@ -29,6 +35,11 @@
     }.
 
 -type bucket() :: klsn:binstr().
+
+-type key() :: klsn:binstr().
+
+%% HTTP method allowed in presigning
+-type http_method() :: get | put | head | delete.
 
 -type s3_owner() :: #{
         id => klsn:binstr()
@@ -50,6 +61,18 @@
         s3_bucket_acl => erlcloud_s3:s3_bucket_acl()
       , s3_location_constraint => erlcloud_s3:s3_location_constraint()
     }.
+
+-type presigned_url() :: klsn:binstr().
+-type make_presigned_url_opts() :: #{
+        bucket := bucket(),
+        key := key(),
+        method => http_method(),      % default: get
+        ttl => ttl(),                 % default: 3600
+        region => klsn:binstr()       % default: <<"us-east-1">>
+    }.
+
+%% time to live (second) from now
+-type ttl() :: non_neg_integer().
 
 -spec create_bucket(
         bucket(), create_bucket_opts(), access_key(), config()
@@ -144,6 +167,77 @@ list_buckets(AKey, Conf) ->
             })
     end.
 
+-spec make_presigned_url(
+        make_presigned_url_opts(), access_key(), config()
+    ) -> presigned_url().
+make_presigned_url(Opts, AKey, Conf) ->
+    AWS = aws_config(AKey, Conf),
+    Bucket = maps:get(bucket, Opts),
+    Key = maps:get(key, Opts),
+    MethodAtom = maps:get(method, Opts, get),
+    TTL = maps:get(ttl, Opts, 3600),
+    RegionBin = maps:get(region, Opts, <<"us-east-1">>),
+    Region = binary_to_list(RegionBin),
+    Method = string:to_upper(atom_to_list(MethodAtom)),
+    Now = erlang:universaltime(),
+    AmzDate = iso8601_basic_time(Now),
+    DateScope = lists:sublist(AmzDate, 8),
+    Service = "s3",
+    SignedHeaders = "host",
+    Host = presign_host(Bucket, AWS),
+    HostWithPort = lists:flatten([Host, port_spec(AWS)]),
+    Path = presign_path(Bucket, Key, AWS),
+    Credential = lists:flatten([
+        AWS#aws_config.access_key_id, $/,
+        DateScope, $/, Region, $/, Service, "/aws4_request"
+    ]),
+    QParams0 = [
+        {"X-Amz-Algorithm", "AWS4-HMAC-SHA256"},
+        {"X-Amz-Credential", Credential},
+        {"X-Amz-Date", AmzDate},
+        {"X-Amz-Expires", integer_to_list(TTL)},
+        {"X-Amz-SignedHeaders", SignedHeaders}
+    ],
+    QParams1 = case AWS#aws_config.security_token of
+        undefined -> QParams0;
+        Token -> QParams0 ++ [{"X-Amz-Security-Token", Token}]
+    end,
+    CanonicalQueryString = canonical_query_string(QParams1),
+    PayloadHash = "UNSIGNED-PAYLOAD",
+    CanonicalHeaders = lists:flatten(["host:", HostWithPort, "\n"]),
+    CanonicalRequest = lists:flatten([
+        Method, $\n,
+        Path, $\n,
+        CanonicalQueryString, $\n,
+        CanonicalHeaders, $\n,
+        SignedHeaders, $\n,
+        PayloadHash
+    ]),
+    CanonicalRequestHash = hash_encode(CanonicalRequest),
+    CredentialScope = lists:flatten([
+        DateScope, $/, Region, $/, Service, "/aws4_request"
+    ]),
+    StringToSign = lists:flatten([
+        "AWS4-HMAC-SHA256\n",
+        AmzDate, $\n,
+        CredentialScope, $\n,
+        CanonicalRequestHash
+    ]),
+    SigningKey = signing_key(DateScope, Region, Service, AWS#aws_config.secret_access_key),
+    SignatureBin = erlcloud_util:sha256_mac(SigningKey, StringToSign),
+    Signature = hex_encode(SignatureBin),
+    FinalQueryString = lists:flatten([
+        CanonicalQueryString,
+        "&X-Amz-Signature=", Signature
+    ]),
+    URL = lists:flatten([
+        AWS#aws_config.s3_scheme,
+        HostWithPort,
+        Path, $?,
+        FinalQueryString
+    ]),
+    klsn_binstr:from_any(URL).
+
 
 %% Internal private functions %%
 
@@ -182,6 +276,62 @@ aws_config(AccessKey, Config) ->
       , s3_bucket_access_method = maps:get(s3_bucket_access_method, Config, path)
       , s3_bucket_after_host = maps:get(s3_bucket_after_host, Config, false)
     }.
+
+iso8601_basic_time({{Yr, Mo, Da}, {H, M, S}}) ->
+    lists:flatten(
+      io_lib:format("~4.10.0b~2.10.0b~2.10.0bT~2.10.0b~2.10.0b~2.10.0bZ",
+                    [Yr, Mo, Da, H, M, S])).
+
+presign_host(Bucket, AWS) ->
+    case AWS#aws_config.s3_bucket_access_method of
+        vhost ->
+            lists:flatten([
+                binary_to_list(Bucket), $.,
+                AWS#aws_config.s3_host
+            ]);
+        _ ->
+            AWS#aws_config.s3_host
+    end.
+
+presign_path(Bucket, Key, AWS) ->
+    EncodedKey = erlcloud_http:url_encode_loose(binary_to_list(Key)),
+    case AWS#aws_config.s3_bucket_access_method of
+        vhost ->
+            lists:flatten(["/", EncodedKey]);
+        _ ->
+            lists:flatten([
+                "/", binary_to_list(Bucket), "/", EncodedKey
+            ])
+    end.
+
+signing_key(DateScope, Region, Service, SecretAccessKey) ->
+    KDate    = erlcloud_util:sha256_mac("AWS4" ++ SecretAccessKey, DateScope),
+    KRegion  = erlcloud_util:sha256_mac(KDate, Region),
+    KService = erlcloud_util:sha256_mac(KRegion, Service),
+    erlcloud_util:sha256_mac(KService, "aws4_request").
+
+canonical_query_string(Params) ->
+    Encoded =
+        [begin
+             KEnc = erlcloud_http:url_encode(K),
+             VEnc = erlcloud_http:url_encode(V),
+             {KEnc, VEnc}
+         end || {K, V} <- Params],
+    Sorted = lists:sort(Encoded),
+    Pairs = [lists:flatten([K1, "=", V1]) || {K1, V1} <- Sorted],
+    string:join(Pairs, "&").
+
+hash_encode(Data) ->
+    Bin = erlang:iolist_to_binary(Data),
+    hex_encode(crypto:hash(sha256, Bin)).
+
+hex_encode(Bin) when is_binary(Bin) ->
+    lists:flatten([io_lib:format("~2.16.0b", [B]) || <<B>> <= Bin]).
+
+port_spec(#aws_config{s3_port = 80}) -> "";
+port_spec(#aws_config{s3_port = 443}) -> "";
+port_spec(#aws_config{s3_port = Port}) ->
+    [":", erlang:integer_to_list(Port)].
 
 owner_field(Key, OwnerPL) ->
     case proplists:get_value(Key, OwnerPL, undefined) of
