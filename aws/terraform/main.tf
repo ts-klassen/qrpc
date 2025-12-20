5545bbf083cbabe10700730f1ba0f03579f7c491
ts-klassen <git@su-shiki.com>
2025-12-21 03:38:15 +0900

    change log days to 30

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -59,7 +59,7 @@ locals {
 
 resource "aws_cloudwatch_log_group" "build" {
   name              = var.log_group_name
-  retention_in_days = 28
+  retention_in_days = 30
   tags              = var.tags
 }
 
