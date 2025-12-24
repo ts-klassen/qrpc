1b892e8e689e09b8869541053a6434ee947773dc
ts-klassen <git@su-shiki.com>
2025-12-25 01:03:39 +0900

    Remove CloudFront custom cache policy

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -114,31 +114,6 @@ resource "aws_cloudfront_origin_access_control" "releases" {
   signing_protocol                  = "sigv4"
 }
 
-resource "aws_cloudfront_cache_policy" "releases" {
-  name        = "${var.project_name}-releases-cache-policy"
-  comment     = "Cache policy for ${var.project_name} releases"
-  default_ttl = 86400
-  max_ttl     = 31536000
-  min_ttl     = 0
-
-  parameters_in_cache_key_and_forwarded_to_origin {
-    cookies_config {
-      cookie_behavior = "none"
-    }
-
-    headers_config {
-      header_behavior = "none"
-    }
-
-    query_strings_config {
-      query_string_behavior = "none"
-    }
-
-    enable_accept_encoding_brotli = true
-    enable_accept_encoding_gzip   = true
-  }
-}
-
 resource "aws_cloudfront_distribution" "releases" {
   enabled             = true
   comment             = "${var.project_name} release distribution"
@@ -161,7 +136,18 @@ resource "aws_cloudfront_distribution" "releases" {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     compress               = true
-    cache_policy_id        = aws_cloudfront_cache_policy.releases.id
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+      headers      = []
+
+      cookies {
+        forward = "none"
+      }
+    }
   }
 
   restrictions {
